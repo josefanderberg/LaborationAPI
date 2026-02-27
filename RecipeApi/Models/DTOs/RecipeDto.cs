238f7b7a150c3e457350c0eb7bdc6175c8bfc6a9
josefanderberg <josef.anderberg@gmail.com>
2026-02-27 10:13:40 +0100

    Initial commit

---
+++ RecipeApi/Models/DTOs/RecipeDto.cs
@@ -0,0 +1,7 @@
+namespace RecipeApi.Models.DTOs;
+
+public class RecipeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ RecipeApi/Program.cs
@@ -0,0 +1,28 @@
+using RecipeApi.Repositories;
+using RecipeApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
