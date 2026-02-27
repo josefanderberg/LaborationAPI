15587f6b9f9c32cc1d6c94d619326255270d76d2
ChloeNkm <nakamanyachloe4@gmail.com>
2026-02-27 11:32:36 +0100

    Re-push

--- RecipeApi/Models/DTOs/RecipeDto.cs
+++ RecipeApi/Models/DTOs/RecipeDto.cs
@@ -3,5 +3,19 @@ namespace RecipeApi.Models.DTOs;
 public class RecipeDto
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public int PrepTimeMinutes { get; set; }
+    public int CookTimeMinutes { get; set; }
+    public int Servings { get; set; }
+    
+    public string Difficulty { get; set; } = string.Empty;
+
+    public List<IngredientDto> Ingredients { get; set; } = new();
+    public List<string> Instructions { get; set; } = new();
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
+//This is a test
