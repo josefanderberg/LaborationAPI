71b291292b4fe22a5c2a744eba4c4320005f011b
ChloeNkm <nakamanyachloe4@gmail.com>
2026-02-27 11:34:42 +0100

    Re-push

--- RecipeApi/Models/DTOs/IngredientDto.cs
+++ RecipeApi/Models/DTOs/IngredientDto.cs
@@ -2,5 +2,12 @@
 {
     public class IngredientDto
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Quantity { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
     }
 }
+
+//This is a test
---
+++ RecipeApi/Models/Ingredient.cs
@@ -0,0 +1,12 @@
+public class Ingredient
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public decimal Quantity { get; set; } = string.Empty;
+    public string Unit { get; set; } = string.Empty;
+
+    public int RecipeId { get; set; }
+    public Recipe Recipe { get; set; }
+}
+//This is a test
